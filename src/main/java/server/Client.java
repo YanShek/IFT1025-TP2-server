d7a79d245d218c42750edce4d63fdb6e6317f3a4
YanShek <yanshek1123@gmail.com>
2023-04-10 19:52:33 -0400

    Update Client.java

--- src/main/java/server/Client.java
+++ src/main/java/server/Client.java
@@ -1,9 +1,13 @@
 package server;
 
+import server.models.Course;
+import server.models.RegistrationForm;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
@@ -23,6 +27,43 @@ public class Client {
 
                 if (commande.equals(("charger").toLowerCase())){
                     System.out.println("Entrez la session");
+                    String session = sc.nextLine();
+
+                    objectOutputStream.writeObject(Server.LOAD_COMMAND + " " + session);
+                    objectOutputStream.flush();
+
+                    ArrayList<Course> cours = (ArrayList<Course>) objectInputStream.readObject();
+                    System.out.println("Voici la liste de cours de la session " +session);
+                    for (Course courses: cours) {
+                        System.out.println(courses.getCode() +" - "+courses.getName());
+                    }
+                } else if (commande.equals(("inscription").toLowerCase())) {
+                    System.out.println("Entrez la session: ");
+                    String session = sc.nextLine();
+                    System.out.println("Entrez le code du cours :");
+                    String code = sc.nextLine();
+                    System.out.println("Entrez votre matricule :");
+                    String matricule = sc.nextLine();
+                    System.out.println("Entrez votre prénom :");
+                    String prenom = sc.nextLine();
+                    System.out.println("Entrez votre nom :");
+                    String nom = sc.nextLine();
+                    System.out.println("Entrez votre email :");
+                    String email = sc.nextLine();
+
+                    RegistrationForm form = new RegistrationForm(prenom, nom, email, matricule, new Course(code, "", session));
+                    ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+
+                    outputStream.writeObject(Server.REGISTER_COMMAND);
+                    outputStream.flush();
+                    outputStream.writeObject(form);
+                    outputStream.flush();
+                    //Message de confirmation
+                    String message = (String) objectInputStream.readObject();
+                    System.out.println();
+                }
+                else{
+                    System.out.println("Instruction invalide");
                 }
             }
         }
